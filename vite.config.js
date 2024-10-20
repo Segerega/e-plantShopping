3e868f576c4cf1be7b20b90803a96ae6abedc5fb
Sergey M <smy@avrillo.co.uk>
2024-10-20 02:01:57 +0100

    Final

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = 'e-plantShopping';
 // https://vitejs.dev/config/
-
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/shoppingreact`,
+  base: `/shoppingreact`,
 });
